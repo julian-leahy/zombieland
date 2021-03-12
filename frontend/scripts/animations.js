1e6bafd5c8a2ec8aa8b64e3cdf2e3d02c4488e81
julian-leahy <julian.leahy.dev@gmail.com>
2021-03-12 16:45:58 +1300

    Add audio for zombie attack

--- frontend/scripts/animations.js
+++ frontend/scripts/animations.js
@@ -1,16 +1,20 @@
 const player1 = document.querySelector('#boy');
 const player2 = document.querySelector('#girl');
+const girlZombie = new Audio('./audio/female-attack.wav');
+const boyZombie = new Audio('./audio/boy-attack.wav');
 
 export const killPlayer1 = () => {
     removeClasses();
     player1.classList.add('dead');
     player2.classList.add('attack');
+    girlZombie.play();
 }
 
 export const killPlayer2 = () => {
     removeClasses();
     player2.classList.add('dead');
     player1.classList.add('attack');
+    boyZombie.play();
 }
 
 export const killBoth = () => {

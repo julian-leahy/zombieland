49a477972f59fc5114ae7b134f4670d97f8fa01c
julian-leahy <julian.leahy.dev@gmail.com>
2021-03-21 12:48:31 +1300

    Update cors origin

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
     cors: {
-        origin: "*",
+        origin: "https://elated-jang-67e207.netlify.app/",
         methods: ["GET", "POST"]
     }
 });

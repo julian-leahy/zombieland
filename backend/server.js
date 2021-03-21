6e55716d997fb95b2ff125f030be79f764c99244
julian-leahy <julian.leahy.dev@gmail.com>
2021-03-21 13:33:01 +1300

    Cors update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
     cors: {
         origin: "*",
-        methods: ["GET", "POST"]
+        methods: "GET"
     }
 });
 const { createRoomName } = require('./utils');

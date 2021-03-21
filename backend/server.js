2ea319c54789d2875e47bf816a401c58c96154b8
julian-leahy <julian.leahy.dev@gmail.com>
2021-03-21 13:23:46 +1300

    Cors test

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
     cors: {
-        origin: "https://glacial-reaches-95195.herokuapp.com/",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
